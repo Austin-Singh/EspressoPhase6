c8fb01b6faf1f718a3c4529ef4f341e8e80d8a73
VinceMack <sentienlvhast@gmail.com>
2022-11-28 22:25:15 -0800

    formatting/minor changes

--- Vince.java
+++ Vince.java
@@ -2,34 +2,6 @@ public class Vince {
 
 	int localParam;
 
-	public static void main(String args[]) {
-
-		boolean var1 = true;
-
-		if (var1) {
-
-			boolean var2 = true;
-			if(var2){
-
-			}
-
-		}else {
-
-			boolean var3 = true;
-			if(var3){
-				if(var3){
-
-				}
-			}
-
-		}
-
-		while(var1){
-			break;
-		}
-
-	}
-
 	Vince(){
 
 	}
@@ -43,14 +15,35 @@ public class Vince {
 	}
 
 	public Vince visitNew(){
-
 		Vince t;
 		Vince t1 = new Vince();
 		Vince t2 = new Vince(420);
-
-		double var6 = 8;
+		double var6 = 8; // ERROR/DIFF NOTE: istore 4 vs istore 
 		//Vince t3 = new Vince(var6);
-
 		return t;
+	}
+
+	public static void main(String args[]) {
+
+		boolean var1 = true;
+
+		if (var1) {
+			boolean var2 = true;
+			if(var2){
+				if(var3){
+
+				}
+			}
+		}else{
+			boolean var3 = true;
+			if(var3){
+				if(var3){
+
+				}
+			}
+		}
+
+		while(var1){break;}
+
 	}
 }
--- test.sh
+++ test.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+ant > /dev/null
 ./espressoc Vince.java > our.txt
 ./espressocr Vince.java > ref.txt
 diff Vince.j Vince.rj > diff.txt
 cat diff.txt
+ant clean > /dev/null
